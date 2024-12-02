0e0cedaa2e5552e4ee0009b4041106f878310467
Babar Mehmood <babarzaidi1122@gmail.com>
2024-12-02 15:15:07 +0500

    commit

---
+++ grid/src/app/components/Grid.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Grid = () => {
+  return (
+    <main className='grid grid-cols-6 grid-rows-5 h-screen gap-2 p-4'>
+      <div className=' col-span-6 bg-cyan-950 text-white flex justify-center items-center'>Header</div>
+      <div className='bg-orange-600 text-white flex justify-center items-center row-span-2'>Left Side Bar</div>
+      <div className='bg-lime-500 text-white flex justify-center items-center col-span-2'>Content 1</div>
+      <div className='bg-lime-500 text-white flex justify-center items-center col-span-2'>Content 2</div>
+      <div className='bg-lime-500 text-white flex justify-center items-center col-span-2'>Content 3</div>
+      <div className='bg-lime-500 text-white flex justify-center items-center col-span-2'>Content 4</div>
+      <div className='bg-orange-600 text-white flex justify-center items-center row-span-2 '>Right Side Bar</div>
+      <div className='bg-cyan-950 text-white flex justify-center items-center col-span-6'>Footer</div>
+    </main>
+  )
+}
+
+export default Grid
